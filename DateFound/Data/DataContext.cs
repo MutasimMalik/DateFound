ec5ad8ef6ca4a467f40ae8eac0b07db82d67f626
Mutasim Malik <malikmutasim2121@gmail.com>
2020-10-28 16:15:30 +0600

    initial commit

---
+++ DateFound/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DateFound.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateFound.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
