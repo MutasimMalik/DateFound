71b9c6a49af938a99bf957bc0c78101236f67c0f
Mutasim Malik <malikmutasim2121@gmail.com>
2020-11-09 00:41:43 +0600

    Client side renovation, resolver and ngx-galley fix

---
+++ DateFound-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { UserService } from 'src/app/_services/user.service';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.scss']
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor(private userService: UserService, private alertify: AlertifyService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    //using resolver to prevent component render before data loading
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+
+    this.galleryOptions = [
+      {
+        width: '428px',
+        height: '428px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+
+    this.galleryImages= this.getImages();
+  }
+
+  getImages() {
+    const imageUrls = [];
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imageUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
+    }
+    return imageUrls;
+  }
+
+  //+ to convert the retrieved id from string to int
+  /*loadUser() {
+    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+      this.user = user;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }*/
+
+}
